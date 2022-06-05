697b862d7e2017566c468cd18c9264d7c66d1572
Joel Benjamin <joel@kafka.local>
2022-06-05 14:14:09 +0530

    loader

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { data } from "../data/images.js";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 
 const defaultTransistion = {
   duration: 1.25,
@@ -18,8 +18,38 @@ const ImageElement = ({ src, id }) => {
   );
 };
 
+const gridUtils = [400, 600, 700, 800, 400];
+
 export default function Home() {
   const [gridVisible, setGridVisible] = useState(true);
+  const loaderControls = useAnimation();
+  const animation = useAnimation();
+
+  useEffect(() => {
+    async function sequence() {
+      await animation.set((index) => {
+        return {
+          y: gridUtils[index % 5],
+          scale: 1.1,
+        };
+      });
+
+      await animation.start((index) => {
+        return {
+          y: 0,
+          transition: defaultTransistion,
+        };
+      });
+
+      setTimeout(() => {
+        setGridVisible(false);
+      }, 200);
+    }
+    setTimeout(() => {
+      sequence();
+    }, 0);
+  }, []);
+
   return (
     <div className="relative w-screen contentHeight bg-blue-600 overflow-hidden cubicTransistion">
       <div className="fixed top-0 p-4 z-30">
@@ -35,11 +65,16 @@ export default function Home() {
           <div className="gridElement">
             {data.map((image, i) => {
               return (
-                <div className="element" key={i}>
+                <motion.div
+                  animate={animation}
+                  className="element"
+                  key={`a-${i}`}
+                  custom={i}
+                >
                   <div className="thumbail">
                     <ImageElement src={image.src} id={i} />
                   </div>
-                </div>
+                </motion.div>
               );
             })}
           </div>
